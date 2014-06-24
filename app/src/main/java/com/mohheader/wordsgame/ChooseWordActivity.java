a414312a18e56a4bbb172c87af9e9f34b07b3c69
Mohammed Header <mohheader@hotmail.com>
2014-06-24 19:50:35 +0300

    init commit

---
+++ app/src/main/java/com/mohheader/wordsgame/ChooseWordActivity.java
@@ -0,0 +1,40 @@
+package com.mohheader.wordsgame;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseWordActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_word);
+
+        List<Word> words = new ArrayList<Word>();
+
+        for(String word : getResources().getStringArray(R.array.words)){
+            Log.i("INFO", "Word  = "+word);
+            int id = getResources().getIdentifier(word, "string", getPackageName());
+            String title = getString(id);
+            int dId = getResources().getIdentifier(word, "drawable", getPackageName());
+
+            words.add(new Word(title,dId));
+        }
+
+        for (Word word:words){
+            Log.i("INFO", "Word Noun = "+word.getTitle());
+            Drawable d = getResources().getDrawable(word.getDrawableResource());
+            ImageView i = new ImageView(this);
+            i.setImageDrawable(d);
+            ((LinearLayout)findViewById(R.id.container)).addView(i);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/mohheader/wordsgame/Word.java
@@ -0,0 +1,20 @@
+package com.mohheader.wordsgame;
+
+/**
+ * Created by mohheader on 6/22/14.
+ */
+public class Word {
+    private String title;
+    private int drawableResource;
+    public Word(String _title, int _drawable_resource){
+        title = _title;
+        drawableResource = _drawable_resource;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public int getDrawableResource() {
+        return drawableResource;
+    }
+}
