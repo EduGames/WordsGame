d006dbd5fd0768a40d3ec8cb73c26b9098544470
Mohammed Header <mohheader@hotmail.com>
2014-07-19 16:54:32 +0200

    Change Text Font,

--- app/src/main/java/com/mohheader/wordsgame/views/WordTextView.java
+++ app/src/main/java/com/mohheader/wordsgame/views/WordTextView.java
@@ -2,6 +2,8 @@ package com.mohheader.wordsgame.views;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import com.mohheader.wordsgame.interfaces.wordable;
  * Created by thedreamer on 7/5/14.
  */
 public class WordTextView extends TextView implements wordable {
+    private static Typeface mTypeface;
     public WordTextView(Context context) {
         this(context, null);
     }
@@ -25,6 +28,11 @@ public class WordTextView extends TextView implements wordable {
         super(context, attrs, defStyle);
         setTextColor(Color.BLUE);
         setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+        if (mTypeface == null) {
+            mTypeface = Typeface.createFromAsset(context.getAssets(), "fonts/shoroq.ttf");
+        }
+        setTypeface(mTypeface);
+        setPaintFlags(Paint.SUBPIXEL_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
     }
 
     @Override
